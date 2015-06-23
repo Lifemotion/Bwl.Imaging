2f922ae6ab1a6d68e1edc2f71cfec4f54cbc1946
Igor Koshelev <igor@lifemotion.ru>
2015-06-23 09:13:34 +0300

    Мелкие изменения.

--- bwl.Imaging.Test/My Project/AssemblyInfo.vb
+++ bwl.Imaging.Test/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' используя "*", как показано ниже:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.15173.224")> 
-<Assembly: AssemblyFileVersion("1.0.15173.224")> 
+<Assembly: AssemblyVersion("1.0.15173.225")> 
+<Assembly: AssemblyFileVersion("1.0.15173.225")> 
