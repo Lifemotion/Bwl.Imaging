26a0f9e66f11a4eac91d474f0fb2d353110e0255
draf <artem.drobanov@gmail.com>
2017-10-06 16:34:21 +0300

    ImageTools.Resize

---
+++ Bwl.Imaging/Tools/ImageTools.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Drawing.Drawing2D
+
+Public Module ImageTools
+    Public Function Resize(img As Bitmap, W As Integer, H As Integer,
+                           Optional align4 As Boolean = True,
+                           Optional interp As InterpolationMode = InterpolationMode.Bilinear) As Bitmap
+        If align4 Then
+            W = If(W Mod 4 <> 0, W + (4 - W Mod 4), W)
+        End If
+        Dim resized = New Bitmap(W, H, PixelFormat.Format24bppRgb)
+        Using gr = Graphics.FromImage(resized)
+            With gr
+                .SmoothingMode = SmoothingMode.None
+                .InterpolationMode = interp
+                .PixelOffsetMode = PixelOffsetMode.Half
+                .DrawImage(img, 0, 0, W, H)
+            End With
+        End Using
+        Return resized
+    End Function
+End Module
