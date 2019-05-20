cfe4927d54f158d3aa9e850044e8714320039985
draf <artem.drobanov@gmail.com>
2019-05-20 12:03:22 +0300

    Очистка.

--- bwl.Imaging/My Project/AssemblyInfo.vb
+++ bwl.Imaging/My Project/AssemblyInfo.vb
@@ -8,17 +8,17 @@ Imports System.Runtime.InteropServices
 
 ' Проверьте значения атрибутов сборки
 
-<Assembly: AssemblyTitle("bwl.Graphics.Common")> 
+<Assembly: AssemblyTitle("Bwl.Imaging")>
 <Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("bwl.Graphics.Common")> 
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Bwl.Imaging")>
 <Assembly: AssemblyCopyright("Copyright ©  2012")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-<Assembly: Guid("5ee8874c-d378-4a96-804d-610f88bd60e1")> 
+<Assembly: Guid("5ee8874c-d378-4a96-804d-610f88bd60e1")>
 
 ' Сведения о версии сборки состоят из следующих четырех значений:
 '
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' используя "*", как показано ниже:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.15173.225")> 
-<Assembly: AssemblyFileVersion("1.0.15173.225")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
 <Assembly: AssemblyInformationalVersion("{c:ymd.} {c:hms:} {chash:6}")>
